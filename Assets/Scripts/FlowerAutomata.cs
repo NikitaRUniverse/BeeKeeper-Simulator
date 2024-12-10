a2658309706eabeb6c431583cd0a43cd011e5d94
Dmtiriy Llama <109825093+dmitriyllama@users.noreply.github.com>
2024-12-10 22:28:14 +0300

    Flower things

---
+++ Assets/Scripts/FlowerAutomata.cs
@@ -0,0 +1,202 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class FlowerAutomata
+{
+    /*
+    Cellular automata in Moore's neighborhood
+
+    -5 to -1 -- Dead flower
+    0 -- Empty space
+    1 to 5 -- Non-pollinating (alive) flower
+    6 to 9 -- Pollinating (alive) flower
+
+    6 to 9
+    Reduces by 1 with "alive reduction chance"
+
+    1 to 5:
+    If at least two neighbors are 6-9, foreach 6-9 neighbor becomes 9 with "pollination chance"
+    Else, reduces by 1 with "alive reduction chance" (if is 1, becomes -1)
+
+    0:
+    If at least two neighbors are 6-9, foreach 6-9 neighbor becomes 5 with "seeding chance"
+
+    -4 до -1:
+    Reduces by 1 with "dead reduction chance"
+
+    -5:
+    Becomes 0
+
+    Rule steps are fired periodically.
+    Bees pollinate alive flowers between steps, turning flowers to 9 immediately.
+    */
+
+
+    private Vector2Int chunksSize;
+    private int chunkToAutomataLength;
+    private int deadSteps;
+    private int autPollenStart;
+    private int autNonPollenStart;
+    private float aliveReductionChance;
+    private float deadReductionChance;
+    private float pollinationChance;
+    private float seedingChance;
+
+
+    private int autDeadStart;
+    private int autDeadLimit;
+    private int autEmpty;
+    private int autBlocked;
+
+    private int[,] chunks;
+    private int[,] automataBuffer;
+    private Vector2Int automataSize;
+
+    public int[,] automata;
+
+    
+    public FlowerAutomata(Vector2Int chunksSize, int chunkToAutomataLength, int deadSteps, int autPollenStart, int autNonPollenStart, float aliveReductionChance, float deadReductionChance, float pollinationChance, float seedingChance)
+    {
+        this.chunksSize = chunksSize;
+        this.chunkToAutomataLength = chunkToAutomataLength;
+        this.deadSteps = deadSteps;
+        this.autPollenStart = autPollenStart;
+        this.autNonPollenStart = autNonPollenStart;
+        this.aliveReductionChance = aliveReductionChance;
+        this.deadReductionChance = deadReductionChance;
+        this.pollinationChance = pollinationChance;
+        this.seedingChance = seedingChance;
+
+        autDeadStart = -1;
+        autDeadLimit = -deadSteps;
+        autEmpty = 0;
+        autBlocked = -8;
+
+        if (autPollenStart <= autNonPollenStart) {
+            Debug.LogError("");
+            return;
+        }
+
+        automataSize = chunksSize * chunkToAutomataLength;
+        ReadChunks("Assets/MapMatrix (8).txt");
+        InitializeAutomata();
+    }
+
+    private void ReadChunks(string filePath)
+    {
+        chunks = new int[chunksSize.x, chunksSize.y];
+        // Read the grid from the file
+        string[] lines = File.ReadAllLines(filePath);
+        chunksSize.y = lines.Length;
+        chunksSize.x = lines[0].Split(' ').Length;
+
+        for (int i = 0; i < lines.Length; i++) {
+            string[] values = lines[i].Split(' ');
+            for (int j = 0; j < values.Length; j++) {
+                chunks[j, i] = int.Parse(values[j]);
+            }
+        }
+    }
+
+    private void InitializeAutomata() {
+        automata = new int[automataSize.x, automataSize.y];
+        
+        for (int chx = 0; chx < chunksSize.x; chx++) for (int chy = 0; chy < chunksSize.y; chy++) {
+            bool blocked = chunks[chx, chy] == 1;
+            for (int dx = 0; dx < chunkToAutomataLength; dx++) for (int dy = 0; dy < chunkToAutomataLength; dy++) {
+                int x = chx*chunkToAutomataLength + dx, y = chy*chunkToAutomataLength + dy;
+                if (blocked) {
+                    automata[x,y] = autBlocked;
+                    continue;
+                }
+            }
+        }
+    }
+
+    public void Step() {
+        automataBuffer = new int[automataSize.x, automataSize.y];
+        
+        for (int x = 1; x < automataSize.x-1; x++) for (int y = 1; y < automataSize.y-1; y++) {
+            // Blocked
+            if (automata[x,y] == autBlocked) {
+                goto AutomataPlainCopy;
+            }
+
+            // About to go empty
+            if (automata[x,y] == autDeadLimit) {
+                automataBuffer[x,y] = autEmpty;
+                continue;
+            }
+
+            bool change;
+
+            // Dying
+            if (automata[x,y] <= autDeadStart) {
+                change = Random.value < deadReductionChance;
+                if (change) {
+                    automataBuffer[x,y] = automata[x,y]-1;
+                    continue;
+                }
+                goto AutomataPlainCopy;
+            }
+
+            // Getting neighbors for other states
+            int pollenAround = 0;
+            for (int dx = x-1; dx < x+2; dx++) for (int dy = y-1; dy < y+2; dy++) {
+                if (dx == x && dy == y) continue;
+                
+                if (automata[dx,dy] > autNonPollenStart) {
+                    pollenAround++;
+                }
+            }
+
+            // Empty
+            if (automata[x,y] == autEmpty) {
+                if (pollenAround > 1) {
+                    change = Random.value > Mathf.Pow(1-seedingChance, pollenAround);
+                    if (change) {
+                        automataBuffer[x,y] = 5;
+                        continue;
+                    }
+                }
+                goto AutomataPlainCopy;
+            }
+
+            // Alive (pollinating or not)
+            if (automata[x,y] <= autPollenStart) {
+                if (pollenAround > 1 && automata[x,y] <= autNonPollenStart) {
+                    change = Random.value > Mathf.Pow(1-pollinationChance, pollenAround);
+                    if (change) {
+                        automataBuffer[x,y] = 9;
+                        continue;
+                    }
+                }
+
+                change = Random.value < aliveReductionChance;
+                if (change) {
+                    if (automata[x,y] == 1) automataBuffer[x,y] = autDeadStart;
+                    else automataBuffer[x,y] = automata[x,y]-1;
+                    continue;
+                }
+                goto AutomataPlainCopy;
+            }
+
+            AutomataPlainCopy:
+            automataBuffer[x,y] = automata[x,y];
+        }
+        automata = automataBuffer;
+    }
+
+    public bool IsEmpty(int x, int y) {
+        return automata[x,y] == autEmpty;
+    }
+
+    public bool IsBlocked(int x, int y) {
+        return automata[x,y] == autBlocked;
+    }
+
+    public bool IsAlive(int x, int y) {
+        return automata[x,y] > autEmpty;
+    }
+}
---
+++ Assets/Scripts/FlowerAutomataSpawner.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using UnityEngine;
+
+public class FlowerAutomataController : MonoBehaviour
+{
+    public GameObject flowerPrefab;
+    public int flowerStartSeeds;
+    public int flowerStartSpreadIters;
+    public float flowerStartSpreadChance;
+    public float flowerCellScale;
+
+    public Vector2Int chunksSize;
+    public int chunkToAutomataLength;
+    public int deadSteps;
+    public int autPollenStart;
+    public int autNonPollenStart;
+    public float aliveReductionChance;
+    public float deadReductionChance;
+    public float pollinationChance;
+    public float seedingChance;
+
+    public bool active;
+    public float stepPeriodSeconds;
+
+    private FlowerAutomata automata;
+
+    
+    private Color color1 = new Color(1f, 0.95f, 0.8f);
+    private Color color2 = new Color(0.7f, 0.7f, 0.75f);
+    private Color color3 = new Color(0.5f, 0.5f, 0.55f);
+    private Color color4 = new Color(0.7f, 0.3f, 0.3f);
+    private Color color5 = new Color(0.2f, 0.6f, 0.4f);
+
+
+    void Start() {
+        automata = new FlowerAutomata(
+            chunksSize,
+            chunkToAutomataLength,
+            deadSteps,
+            autPollenStart,
+            autNonPollenStart,
+            aliveReductionChance,
+            deadReductionChance,
+            pollinationChance,
+            seedingChance
+        );
+        for (int i = 0; i < flowerStartSeeds; i++) {
+            SpawnFlowerRandomly(); 
+        }
+        for (int i = 0; i < flowerStartSpreadIters; i++) {
+            SpreadFlowersRandomly();
+        }
+        // StartCoroutine(AutomataLoop());
+    }
+
+    void SpawnFlowerRandomly() {
+        int x,y;
+        while (true) {
+            x = Random.Range(0, automata.automata.GetLength(0));
+            y = Random.Range(0, automata.automata.GetLength(1));
+            if (automata.automata[x,y] == 0) break;
+        }
+
+        SpawnFlower(x,y,9);
+    }
+
+    void SpawnFlower(int x, int y, int automatonValue) {
+        automata.automata[x,y] = automatonValue;
+
+        // TODO random spawn offsets
+        Vector3 pos = new Vector3(x, 0, y) * flowerCellScale;
+
+        GameObject newFlower = Instantiate(flowerPrefab, pos, Quaternion.identity);
+        Color randomColor = NewFlowerColor();
+        Renderer flowerRenderer = newFlower.GetComponent<Renderer>();
+        if (flowerRenderer != null)
+        {
+            flowerRenderer.material.color = randomColor;
+        }    
+    }
+
+    void SpreadFlowersRandomly() {
+        for (int x = 1; x < automata.automata.GetLength(0)-1; x++) for (int y = 1; y < automata.automata.GetLength(1)-1; y++) {
+            if (!automata.IsEmpty(x,y)) continue;
+            for (int dx = x-1; dx < x+2; dx++) for (int dy = y-1; dy < y+2; dy++) {
+                if (automata.IsAlive(x,y)) {
+                    bool spawn = Random.value < flowerStartSpreadChance;
+                    if (spawn) SpawnFlower(x,y,9);
+                }
+            }
+        }
+    }
+
+    Color NewFlowerColor() {
+        int randomIndex = Random.Range(0, 5); 
+        switch (randomIndex) {
+            case 0:
+                return color1;
+            case 1:
+                return color2;
+            case 2:
+                return color3;
+            case 3:
+                return color4;
+            case 4:
+                return color5;
+            default:
+                return color1;
+        }
+    }
+
+    IEnumerator AutomataLoop() {
+        while (active) {
+            automata.Step();
+            // TODO update flowers
+            yield return new WaitForSeconds(stepPeriodSeconds);
+        }
+    }
+
+    void PollinateFlower(int x, int y) {
+        automata.automata[x,y] = 9;
+        // TODO do something cool to the flower
+    }
+}
